d3a81d905034c41229eded66a05869e7c3446c70
geeet05 <113108880+geeet05@users.noreply.github.com>
2024-10-31 19:50:11 +0530

    4

--- models/userD.js
+++ models/userD.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose')
 
 mongoose.connect('mongodb+srv://geetsahu1852005:80UfZp6Pf3pzqNQ9@sihcluster.1zgai.mongodb.net/?retryWrites=true&w=majority&appName=SIHCluster')
+  .then(() => {
+    console.log('MongoDB connected successfully');
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+  });
 
 const userSchema = mongoose.Schema({
     userID: String,
