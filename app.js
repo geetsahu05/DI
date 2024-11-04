dd43a9d011300cca89b4603ab873c33be1a7e5d4
geeet05 <113108880+geeet05@users.noreply.github.com>
2024-11-05 01:22:14 +0530

    22

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 //const bcrypt = require('bcrypt');
+//Render and vercel
 const bcrypt = require('bcryptjs');
 
 const jwt = require('jsonwebtoken');
